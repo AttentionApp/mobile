ec8681bde95951cec6a2aded58f95cab0a96e713
Walter Jean Pierre Huapaya Chavez <jeanpierrehuapaya@gmail.com>
2019-09-18 17:35:58 -0500

    Crete HomeActivity

---
+++ app/src/main/java/pe/edu/upc/attentionapp/controllers/activities/HomeActivity.kt
@@ -0,0 +1,46 @@
+package pe.edu.upc.attentionapp.controllers.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.MenuItem
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import pe.edu.upc.attentionapp.R
+import pe.edu.upc.attentionapp.controllers.fragments.HomeFragment
+
+class HomeActivity : AppCompatActivity() {
+
+    private val onNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        navigateTo(item)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+
+        val navView: BottomNavigationView = findViewById(R.id.nav_view)
+
+        navView.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener)
+        navigateTo(navView.menu.findItem(R.id.navigation_home))
+    }
+
+    private fun getFragmentFor(item: MenuItem): Fragment {
+        return when(item.itemId) {
+            R.id.navigation_home -> HomeFragment()
+            /*R.id.navigation_sources -> SourcesFragment()
+            R.id.navigation_favorites -> FavoritesFragment()
+            R.id.navigation_settings -> SettingsFragment()
+            */else -> HomeFragment()
+        }
+    }
+
+    private fun navigateTo(item: MenuItem): Boolean {
+        item.isChecked = true
+
+        return supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.content, getFragmentFor(item))
+            .commit() > 0
+    }
+
+}
---
+++ app/src/main/java/pe/edu/upc/attentionapp/controllers/fragments/HomeFragment.kt
@@ -0,0 +1,6 @@
+package pe.edu.upc.attentionapp.controllers.fragments
+
+import androidx.fragment.app.Fragment
+
+class HomeFragment: Fragment() {
+}
