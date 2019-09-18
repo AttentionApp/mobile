80d126b02b2cf5a28d99a35fa8fd2973cb2d78b8
Manuel Alvarado Estanga <alvarado.manuel@live.com>
2019-09-18 13:35:56 -0500

    Added nurses available activity

---
+++ app/src/main/java/pe/edu/upc/attentionapp/controllers/activities/NursesAvailable.kt
@@ -0,0 +1,12 @@
+package pe.edu.upc.attentionapp.controllers.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NursesAvailable : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_nurse_availables)
+    }
+}
